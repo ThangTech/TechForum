b61a2c23759e7dcaefde6aa049ec71bcc6919960
Van Thanggit config --global user.email nguyenvanthangktpm@gmail.comgit config --global user.name Van <nguyenvanthangktpm@gmail.com>
2026-09-07 20:20:39 +0700

    initialize backend structure with API and configuration files

---
+++ backend/Controllers/TestController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TechForum.Api.Controllers
+{
+    [Route("api/test")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(new
+            {
+                status = "ok",
+                application = "TechForum.Api",
+                timestamp = DateTimeOffset.UtcNow
+            });
+        }
+    }
+}
